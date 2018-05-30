0184b634d472ea070ffe87785c7f9fa9cec6a87f
Usuário <usuario@MacBook-Pro-de-Usuario-2.local>
2018-05-30 00:20:21 -0300

    ...

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+const app = express();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.json({users: [{name: 'Timmy'}]});
+  res.send('ok')
 });
 
 module.exports = router;
